91bcc0201a8ec8d65d3b0a6a23b9921c34005d2f
Ruzsa Balazs <balazs.ruzsa@disney.com>
2014-10-14 16:04:13 +0200

    new lib: nanomsg

---
+++ zzlua/tests/nanomsg.lua
@@ -0,0 +1,28 @@
+local nn = require('nanomsg')
+local ffi = require('ffi')
+local time = require('time')
+local sf = string.format
+
+local function isnumber(x)
+   return type(x) == "number"
+end
+
+local sub_sock = nn.socket(nn.AF_SP, nn.SUB)
+nn.setsockopt(sub_sock, nn.SUB, nn.SUB_SUBSCRIBE, "")
+nn.connect(sub_sock, "inproc://messages")
+
+local pub_sock = nn.socket(nn.AF_SP, nn.PUB)
+nn.bind(pub_sock, "inproc://messages") -- bind the PUB, connect to SUB
+nn.send(pub_sock, "hello")
+
+local poll = nn.Poll()
+poll:add(sub_sock, nn.POLLIN) -- socket, events
+assert(#poll.items == 1)
+local nevents = poll(-1) -- timeout in ms, -1=block
+assert(nevents == 1, sf("poll() returned nevents=%d, expected 1", nevents))
+assert(poll[0].revents == nn.POLLIN)
+local buf = nn.recv(sub_sock)
+assert(buf=="hello")
+
+nn.close(pub_sock)
+nn.close(sub_sock)
