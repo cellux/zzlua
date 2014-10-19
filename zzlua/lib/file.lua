b3f722d2ab2501cbd57c2f36fe06c22f4d073daf
Ruzsa Balazs <cellux@omkamra.hu>
2014-10-19 11:21:49 +0200

    stat and chmod

--- zzlua/lib/file.lua
+++ zzlua/lib/file.lua
@@ -3,13 +3,37 @@ local sf = string.format
 
 ffi.cdef [[
 
-int open (const char *__file, int __oflag, ...);
+int     open (const char *__file, int __oflag, ...);
 ssize_t read (int __fd, void *__buf, size_t __nbytes);
-int close (int __fd);
-
-int access(const char *pathname, int mode);
-
+int     close (int __fd);
+int     access (const char *pathname, int mode);
 __off_t lseek (int __fd, __off_t __offset, int __whence);
+int     chmod (const char *__file, __mode_t __mode);
+
+struct Stat_ct {
+  struct stat *buf;
+};
+
+struct stat *     zzlua_Stat_new();
+__dev_t           zzlua_Stat_dev(struct stat *);
+__ino_t           zzlua_Stat_ino(struct stat *);
+__mode_t          zzlua_Stat_mode(struct stat *);
+__mode_t          zzlua_Stat_type(struct stat *buf);
+__mode_t          zzlua_Stat_perms(struct stat *buf);
+__nlink_t         zzlua_Stat_nlink(struct stat *);
+__uid_t           zzlua_Stat_uid(struct stat *);
+__gid_t           zzlua_Stat_gid(struct stat *);
+__dev_t           zzlua_Stat_rdev(struct stat *);
+__off_t           zzlua_Stat_size(struct stat *);
+__blksize_t       zzlua_Stat_blksize(struct stat *);
+__blkcnt_t        zzlua_Stat_blocks(struct stat *);
+struct timespec * zzlua_Stat_atime(struct stat *);
+struct timespec * zzlua_Stat_mtime(struct stat *);
+struct timespec * zzlua_Stat_ctime(struct stat *);
+void              zzlua_Stat_free(struct stat *);
+
+void zzlua_stat(const char *pathname, struct stat *buf);
+void zzlua_lstat(const char *pathname, struct stat *buf);
 
 struct File_ct {
   int fd;
@@ -65,6 +89,84 @@ File_mt.__gc = File_mt.close
 
 local File = ffi.metatype("struct File_ct", File_mt)
 
+local Stat_mt = {}
+
+function Stat_mt:stat(path)
+   return ffi.C.zzlua_stat(path, self.buf)
+end
+
+function Stat_mt:lstat(path)
+   return ffi.C.zzlua_lstat(path, self.buf)
+end
+
+local Stat_accessors = {
+   dev = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_dev(buf))
+   end,
+   ino = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_ino(buf))
+   end,
+   mode = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_mode(buf))
+   end,
+   perms = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_perms(buf))
+   end,
+   type = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_type(buf))
+   end,
+   nlink = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_nlink(buf))
+   end,
+   uid = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_uid(buf))
+   end,
+   gid = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_gid(buf))
+   end,
+   rdev = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_rdev(buf))
+   end,
+   size = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_size(buf))
+   end,
+   blksize = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_blksize(buf))
+   end,
+   blocks = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_blocks(buf))
+   end,
+   atime = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_atime(buf).tv_sec)
+   end,
+   mtime = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_mtime(buf).tv_sec)
+   end,
+   ctime = function(buf)
+      return tonumber(ffi.C.zzlua_Stat_ctime(buf).tv_sec)
+   end,
+}
+
+function Stat_mt:__index(key)
+   local accessor = Stat_accessors[key]
+   if accessor then
+      return accessor(self.buf)
+   else
+      local field = rawget(Stat_mt, key)
+      if field then
+         return field
+      else
+         error(sf("invalid key: %s, no such field in struct stat", key))
+      end
+   end
+end
+
+function Stat_mt:__gc()
+   ffi.C.zzlua_Stat_free(self.buf)
+end
+
+local Stat = ffi.metatype("struct Stat_ct", Stat_mt)
+
 local M = {}
 
 function M.open(path)
@@ -81,6 +183,22 @@ end
 
 function M.exists(path)
    return ffi.C.access(path, F_OK) == 0
+end
+
+function M.stat(path)
+   local s = Stat(ffi.C.zzlua_Stat_new())
+   s:stat(path)
+   return s
+end
+
+function M.lstat(path)
+   local s = Stat(ffi.C.zzlua_Stat_new())
+   s:lstat(path)
+   return s
+end
+
+function M.chmod(path, mode)
+   return ffi.C.chmod(path, mode)
 end
 
 local M_mt = {
--- zzlua/tests/file.lua
+++ zzlua/tests/file.lua
@@ -1,8 +1,42 @@
 local file = require('file')
+local time = require('time')
+local assert = require('assert')
+local sf = string.format
 
 -- exists
 assert(file.exists('testdata/hello.txt'))
 assert(not file.exists('non-existing-file'))
+
+-- chmod
+local function oct(s) return tonumber(s, 8) end
+file.chmod("testdata/hello.txt", oct("755"))
+assert.equals(file.stat("testdata/hello.txt").perms, oct("755"))
+file.chmod("testdata/hello.txt", oct("644"))
+assert(file.stat("testdata/hello.txt").perms == oct("644"))
+
+-- stat
+local s = file.stat("testdata/hello.txt")
+assert.type(s.dev, 'number')
+assert.type(s.ino, 'number')
+assert.type(s.mode, 'number')
+assert.type(s.perms, 'number')
+assert.type(s.type, 'number')
+assert.equals(s.mode, s.perms + s.type)
+assert.type(s.nlink, 'number')
+assert.type(s.uid, 'number')
+assert.type(s.gid, 'number')
+assert.type(s.rdev, 'number')
+assert.type(s.size, 'number')
+assert.equals(s.size, 14, "stat('testdata/hello.txt').size")
+assert.type(s.blksize, 'number')
+assert.type(s.blocks, 'number')
+assert.type(s.atime, 'number')
+assert.type(s.mtime, 'number')
+assert.type(s.ctime, 'number')
+
+-- "The field st_ctime is changed by writing or by setting inode
+-- information (i.e., owner, group, link count, mode, etc.)."
+assert(math.abs(time.time()-s.ctime) < 1, sf("time.time()=%s, s.ctime=%s, difference > 1 seconds", time.time(), s.ctime))
 
 -- read whole file at once
 local f = file('testdata/hello.txt')
