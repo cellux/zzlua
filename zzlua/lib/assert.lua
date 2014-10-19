3495cba671fe614dbb681ba1412beb8346081407
Ruzsa Balazs <cellux@omkamra.hu>
2014-10-19 19:20:20 +0200

    assert improvements

--- zzlua/lib/assert.lua
+++ zzlua/lib/assert.lua
@@ -2,34 +2,44 @@ local sf = string.format
 
 local M = {}
 
-local function assert_type(x, t, name_of_x)
+local function assert_true(x, err, level)
+   err = err or "assertion failed!"
+   level = level or 2
+   if x ~= true then
+      error(err, level)
+   end
+end
+
+local function assert_type(x, t, name_of_x, level)
+   level = level or 2
    if name_of_x then
-      assert(type(x)==t, sf("type(%s)==%s, expected %s", name_of_x, type(x), t))
+      assert_true(type(x)==t, sf("type(%s)==%s, expected %s", name_of_x, type(x), t), level+1)
    else
-      assert(type(x)==t, sf("type(%s)==%s, expected %s", tostring(x), type(x), t))
+      assert_true(type(x)==t, sf("type(%s)==%s, expected %s", tostring(x), type(x), t), level+1)
    end
 end
 
 M.type = assert_type
 
-local function assert_equals(x, y, name_of_x)
-   assert(type(x) == type(y), sf("x.type (%s) != y.type (%s)", type(x), type(y)))
+local function assert_equals(x, y, name_of_x, level)
+   level = level or 2
+   assert_true(type(x) == type(y), sf("x.type (%s) != y.type (%s)", type(x), type(y)), level+1)
    if type(x)=="table" then
-      assert(#x==#y)
+      assert_true(#x==#y, sf("#x=%d != #y=%d", #x, #y), level+1)
       for i=1,#x do
-         assert_equals(x[i], y[i])
+         assert_equals(x[i], y[i], name_of_x and sf("%s[%d]", name_of_x, i), level+1)
       end
       for k,v in pairs(x) do
-         assert_equals(x[k], y[k])
+         assert_equals(x[k], y[k], name_of_x and sf("%s[%s]", name_of_x, k), level+1)
       end
       for k,v in pairs(y) do
-         assert_equals(y[k], x[k])
+         assert_equals(y[k], x[k], name_of_x, level+1)
       end
    else
       if name_of_x then
-         assert(x==y, sf("%s is %s, expected %s", name_of_x, tostring(x), tostring(y)))
+         assert_true(x==y, sf("%s is %s, expected %s", name_of_x, tostring(x), tostring(y)), level+1)
       else
-         assert(x==y, sf("%s != %s", tostring(x), tostring(y)))
+         assert_true(x==y, sf("%s != %s", tostring(x), tostring(y)), level+1)
       end
    end
 end
@@ -38,8 +48,8 @@ M.equals = assert_equals
 
 local M_mt = {}
 
-function M_mt:__call(...)
-   assert(...)
+function M_mt:__call(x, err)
+   assert_true(x ~= nil and x ~= false, err, 3)
 end
 
 return setmetatable(M, M_mt)
