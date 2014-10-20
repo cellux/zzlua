f3b5c46cc84366c515613a8e28a31d49f21ef87e
Ruzsa Balazs <balazs.ruzsa@disney.com>
2014-10-20 15:31:22 +0200

    moved stuff from jack.h to jack.c (no others users)

--- zzlua/lib/jack.c
+++ zzlua/lib/jack.c
@@ -10,7 +10,20 @@
 #include "msgpack.h" /* cmp */
 #include "nanomsg/nn.h"
 
-#include "jack.h"
+#define ZZ_JACK_PORT_AUDIO 1
+#define ZZ_JACK_PORT_MIDI  2
+
+#define ZZ_JACK_PORTS_MAX  32
+
+struct zz_jack_params {
+  jack_client_t *client;
+  jack_ringbuffer_t *midi_rb;
+  jack_port_t* ports[ZZ_JACK_PORTS_MAX];
+  int port_types[ZZ_JACK_PORTS_MAX];
+  int port_flags[ZZ_JACK_PORTS_MAX];
+  int nports;
+  int event_socket;
+};
 
 #define SCRATCH_SIZE 1024
 
