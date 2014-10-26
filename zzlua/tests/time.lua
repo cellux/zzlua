8420418d3c9ddba38ad5915698ead31f89d933af
Ruzsa Balazs <cellux@omkamra.hu>
2014-10-26 13:36:32 +0100

    test time.sleep() in both sync and async context

--- zzlua/tests/time.lua
+++ zzlua/tests/time.lua
@@ -1,18 +1,23 @@
 local time = require('time')
 local sched = require('sched')
 
--- sync
+local function test_sleep()
+   local t1 = time.time()
+   local sleep_time = 0.1
+   time.sleep(sleep_time)
+   local t2 = time.time()
+   local elapsed = t2 - t1
+   local diff = math.abs(elapsed - sleep_time)
+   local max_diff = 1e-3 -- we expect millisecond precision
+   assert(diff < max_diff, string.format("there are problems with timer precision: diff (%f) >= max allowed diff (%f)", diff, max_diff))
+end
 
-local t1 = time.time()
-local sleep_time = 0.1
-time.sleep(sleep_time)
-local t2 = time.time()
-local elapsed = t2 - t1
-local diff = math.abs(elapsed - sleep_time)
-local max_diff = 1e-3 -- we expect millisecond precision
-assert(diff < max_diff, string.format("there are problems with timer precision: diff (%f) >= max allowed diff (%f)", diff, max_diff))
+-- sync 
+test_sleep()
 
 -- async
+sched(test_sleep)
+sched()
 
 local coll = {}
 
