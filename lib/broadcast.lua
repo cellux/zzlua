a83701b89665d18ea7747e596d00a451e165e513
Ruzsa Balazs <cellux@omkamra.hu>
2015-09-02 06:22:34 +0200

    broadcast tests (not working yet)

--- lib/broadcast.lua
+++ lib/broadcast.lua
@@ -1,3 +1,204 @@
+local sched = require('sched')
+local socket = require('socket')
+local nn = require('nanomsg')
+local msgpack = require('msgpack')
+local net = require('net')
+local env = require('env')
+local sys = require('sys')
+local adt = require('adt')
+local inspect = require('inspect')
+local sf = string.format
+local time = require('time')
+
 local M = {}
 
-return M
+local broadcast_port = tonumber(env.ZZ_BROADCAST_PORT or 3532)
+
+local any_addr = socket.sockaddr(socket.AF_INET, "0.0.0.0", broadcast_port)
+local broadcast_addr = socket.sockaddr(socket.AF_INET, "255.255.255.255", broadcast_port)
+local local_addr = socket.sockaddr(socket.AF_INET, "127.0.0.1", broadcast_port)
+
+local broadcast_socket = nil -- for broadcasting to network peers
+local broadcast_socket_addr = nil -- sockaddr of outgoing socket
+
+local event_address = sf("tcp://127.0.0.1:%u", broadcast_port)
+
+local cbregistry = nil -- subscriber callbacks
+
+local initialized = false
+
+M.OFF = {}
+
+local function CallbackRegistry(invoke_fn)
+   local self = {}
+   local callbacks = {}
+   function self.on(evtype, cb)
+      if not callbacks[evtype] then
+         callbacks[evtype] = adt.List()
+      end
+      callbacks[evtype]:push(cb)
+   end
+   function self.off(evtype, cb)
+      if callbacks[evtype] then
+         local cbs = callbacks[evtype]
+         local i = cbs:index(cb)
+         if i then
+            cbs:remove_at(i)
+         end
+         if callbacks[evtype]:empty() then
+            callbacks[evtype] = nil
+         end
+      end
+   end
+   function self.emit(evtype, ...)
+      local cbs = callbacks[evtype]
+      if cbs then
+         for cb in cbs:itervalues() do
+            invoke_fn(cb, evtype, ...)
+         end
+      end
+   end
+   return self
+end
+
+function M.broadcast(evtype, evdata, dest_addr)
+   if not initialized then
+      sched.wait('broadcast.initialized')
+      initialized = true
+   end
+   evdata = evdata or 0
+   dest_addr = dest_addr or broadcast_addr
+   print(sys.getpid(), "broadcast:", evtype, evdata)
+   local msg = msgpack.pack_array({evtype, evdata})
+   broadcast_socket:sendto(msg, dest_addr)
+end
+
+local function listener()
+   local s = socket(socket.PF_INET, socket.SOCK_DGRAM)
+   local rv, err = pcall(s.bind, s, any_addr)
+   if rv then
+      -- bind successful
+      --
+      -- proxy incoming events to broadcast subscribers on this host
+      local event_pub = nn.socket(nn.AF_SP, nn.PUB)
+      print(sys.getpid(), "connecting event_pub to", event_address)
+      nn.connect(event_pub, event_address)
+      -- poll the listener socket for incoming events
+      net.qpoll(s.fd, function()
+         local msg, peer_addr = s:recvfrom()
+         local sender_address = peer_addr.address
+         local sender_port = peer_addr.port
+         local unpacked = msgpack.unpack(msg)
+         assert(type(unpacked) == "table")
+         assert(#unpacked == 2, "broadcast message shall be a table of two elements, but it is "..inspect(unpacked))
+         local evtype, evdata = unpack(unpacked)
+         print(sys.getpid(), "listener:", evtype, evdata, sender_address, sender_port)
+         nn.send(event_pub, msgpack.pack_array({evtype, evdata, sender_address, sender_port}))
+      end)
+      nn.close(event_pub)
+   end
+   s:close()
+end
+
+local function subscriber()
+   local event_sub = nn.socket(nn.AF_SP, nn.SUB)
+   nn.setsockopt(event_sub, nn.SUB, nn.SUB_SUBSCRIBE, "")
+   print(sys.getpid(), "binding event_sub to", event_address)
+   nn.bind(event_sub, event_address)
+   local event_sub_fd = nn.getsockopt(event_sub, 0, nn.RCVFD)
+   assert(event_sub_fd > 0)
+   -- prime the subscriber
+   local ping_attempts = 10
+   local got_ping = false
+   sched(function()
+      while not got_ping do
+         print(sys.getpid(), "before ping poll", event_sub_fd)
+         sched.poll(event_sub_fd, "r")
+         print(sys.getpid(), "after ping poll", event_sub_fd)
+         local msg = nn.recv(event_sub)
+         local unpacked = msgpack.unpack(msg)
+         assert(type(unpacked) == "table")
+         assert(#unpacked == 4, inspect(unpacked))
+         local evtype, evdata, sender_address, sender_port = unpack(unpacked)
+         print(sys.getpid(), "subscriber init:", evtype, evdata, sender_address, sender_port)
+         if evtype == 'broadcast.ping' and sender_address == M.sender_address and sender_port == M.sender_port then
+            print(sys.getpid(), "got ping")
+            got_ping = true
+         end
+      end
+   end)
+   while not got_ping and ping_attempts > 0 do
+      local msg = msgpack.pack_array({'broadcast.ping', 0})
+      print(sys.getpid(), "sending broadcast.ping to local_addr from", M.sender_address, M.sender_port)
+      broadcast_socket:sendto(msg, local_addr)
+      ping_attempts = ping_attempts - 1
+      sched.sleep(0.1)
+   end
+   if got_ping then
+      print(sys.getpid(), "broadcast.initialized")
+      sched.emit('broadcast.initialized', 0)
+   else
+      error("cannot wire up broadcast subscriber to listener")
+   end
+   -- wait for events
+   net.qpoll(event_sub_fd, function()
+       local msg = nn.recv(event_sub)
+       local unpacked = msgpack.unpack(msg)
+       assert(type(unpacked) == "table")
+       assert(#unpacked == 4, inspect(unpacked))
+       local evtype, evdata, sender_address, sender_port = unpack(unpacked)
+       print(sys.getpid(), "subscriber :", evtype, evdata, sender_address, sender_port)
+       cbregistry.emit(evtype, evdata, sender_address, sender_port)
+   end)
+   nn.close(event_sub)
+end
+
+local function BroadcastModule(sched)
+   local function invoke_subscriber_callback(cb, evtype, evdata, sender_address, sender_port)
+      local function wrapper()
+         local rv = cb(evdata, sender_address, sender_port)
+         if rv == M.OFF then
+            M.off(evtype, cb)
+         end
+      end
+      sched.sched(wrapper)
+   end
+   local self = {}
+   function self.init()
+      initialized = false
+      broadcast_socket = socket(socket.PF_INET, socket.SOCK_DGRAM)
+      broadcast_socket.SO_BROADCAST = true
+      broadcast_socket:connect(broadcast_addr)
+      broadcast_socket_addr = broadcast_socket:getsockname()
+      M.sender_address = broadcast_socket_addr.address
+      M.sender_port = broadcast_socket_addr.port
+      cbregistry = CallbackRegistry(invoke_subscriber_callback)
+      M.on = cbregistry.on
+      M.off = cbregistry.off
+      sched.background(listener)
+      sched.background(subscriber)
+   end
+   function self.done()
+      M.on = nil
+      M.off = nil
+      cbregistry = nil
+      broadcast_socket:close()
+      M.sender_port = nil
+      M.sender_address = nil
+      broadcast_socket_addr = nil
+      broadcast_socket = nil
+      initialized = false
+      print(sys.getpid(), "broadcast:done")
+   end
+   return self
+end
+
+sched.register_module(BroadcastModule)
+
+local M_mt = {}
+
+function M_mt:__call(...)
+   return self.broadcast(...)
+end
+
+return setmetatable(M, M_mt)
--- tests/broadcast.lua
+++ tests/broadcast.lua
@@ -1,23 +1,94 @@
 local broadcast = require('broadcast')
 local sys = require('sys')
+local assert = require('assert')
+local sched = require('sched')
 
 -- the broadcast module provides an UDP-based message distribution
 -- facility which lets you send messages to all zzlua processes
 -- running anywhere on the local IP network.
 --
--- to receive broadcast messages, you need exactly one broadcast
--- listener per host who distributes incoming broadcast messages to
--- local subscribers in a pubsub fashion.
+-- to receive broadcast messages, you need exactly one *broadcast
+-- listener* per host who distributes incoming broadcast messages to
+-- local *broadcast subscribers* in a pubsub fashion.
 --
 -- the broadcast module registers itself as a scheduler module. when
 -- you call sched(), the module checks whether a listener is already
--- running at the configured broadcast port (default: udp 3532, may be
+-- running on the configured broadcast port (default: UDP 3532, may be
 -- overridden by setting the ZZ_BROADCAST_PORT environment variable)
 -- and if not, it starts one automatically.
 --
 -- when a message arrives, the listener publishes it on a nanomsg PUB
--- socket at tcp://localhost:3532. (the port number is the same as
--- that used for receiving broadcast messages, just TCP instead of
--- UDP.) broadcast-enabled schedulers automatically subscribe to this
--- TCP port and convert incoming broadcast messages to standard
--- scheduler events.
+-- socket at tcp://localhost:3532. the port number is the same as that
+-- used for receiving broadcast messages, just TCP instead of UDP.
+-- zzlua processes using the broadcast module automatically poll this
+-- TCP port for messages and distribute them to all registered
+-- callbacks.
+
+-- requiring the broadast module and starting the scheduler causes a
+-- broadcast subscriber (and potentially a broadcast listener) to be
+-- set up in the current process
+
+-- this test is not working yet
+sys.exit()
+
+sched()
+
+print("---snip---")
+
+print(sys.getpid(), "parent")
+local pid, sp = sys.fork(function(sc)
+      sched(function()
+            print(sys.getpid(), "child")
+            sched.wait("broadcast.initialized")
+            print(sys.getpid(), "child got broadcast.initialized")
+            sc:write("ready\n")
+            assert.equals(sc:readline(), "stop")
+      end)
+      sched()
+end)
+sched(function()
+   assert.equals(sp:readline(), "ready")
+   print(sys.getpid(), "got ready from child")
+   sp:write("stop\n")
+end)
+sched()
+sp:close()
+sys.waitpid(pid)
+
+print("---snip---")
+
+
+-- broadcasting events from process A
+-- subscribing to them in process B
+
+local pid, sp = sys.fork(function(sc)
+      local messages = {}
+      sched(function()
+            broadcast.on('broadcast-test', function(evdata)
+               table.insert(messages, evdata)
+            end)
+            broadcast.on('broadcast-quit', function()
+               sched.emit('broadcast-quit')
+            end)
+            print(sys.getpid(), "BROADCAST INITIALIZED BEFORE")
+            sched.wait('broadcast.initialized')
+            print(sys.getpid(), "BROADCAST INITIALIZED AFTER")
+            sc:write("ready\n")
+            print(sys.getpid(), "waiting for broadcast-quit")
+            sched.wait('broadcast-quit')
+            print(sys.getpid(), "got broadcast-quit")
+      end)
+      sched()
+      assert.equals(messages, {'hello','world'})
+end)
+
+sched(function()
+   assert.equals(sp:readline(), "ready")
+   broadcast('broadcast-test', 'hello')
+   broadcast('broadcast-test', 'world')
+   broadcast('broadcast-quit')
+end)
+sched()
+
+sp:close()
+sys.waitpid(pid)
