bac100636efe8d58da23c18bd9c7685e01cdbeb8
Ruzsa Balazs <cellux@omkamra.hu>
2015-09-02 06:21:32 +0200

    tcp test between two processes

--- tests/nanomsg.lua
+++ tests/nanomsg.lua
@@ -2,6 +2,8 @@ local nn = require('nanomsg')
 local ffi = require('ffi')
 local time = require('time')
 local sched = require('sched')
+local sys = require('sys')
+local assert = require('assert')
 local sf = string.format
 
 local function isnumber(x)
@@ -90,3 +92,49 @@ sched(function()
 end)
 
 sched()
+
+-- tcp + wait between two processes
+
+local pid, sp = sys.fork(function(sc)
+   sched(function()
+      local sub_sock = nn.socket(nn.AF_SP, nn.SUB)
+      nn.setsockopt(sub_sock, nn.SUB, nn.SUB_SUBSCRIBE, "")
+      nn.bind(sub_sock, "tcp://127.0.0.1:54321")
+      -- wait for the first message to arrive
+      local sub_sock_fd = nn.getsockopt(sub_sock, 0, nn.RCVFD)
+      sched.poll(sub_sock_fd, "r")
+      assert(nn.recv(sub_sock)=="ping")
+      sc:write("got ping\n")
+      -- from now on, all messages will be delivered (but pub/sub is
+      -- inherently unreliable, so in theory, there is no guarantee)
+      local messages = {}
+      for i=1,10 do
+         sched.poll(sub_sock_fd, "r")
+         table.insert(messages, nn.recv(sub_sock))
+      end
+      assert(#messages==10)
+      nn.close(sub_sock)
+   end)
+   sched()
+end)
+
+sched(function()
+   local pub_sock = nn.socket(nn.AF_SP, nn.PUB)
+   nn.connect(pub_sock, "tcp://127.0.0.1:54321")
+   local setup_done = false
+   sched(function()
+      assert.equals(sp:readline(), "got ping")
+      setup_done = true
+   end)
+   while not setup_done do
+      nn.send(pub_sock, "ping")
+      sched.sleep(0.1)
+   end
+   for i=1,10 do
+      nn.send(pub_sock, sf("msg-%d", i))
+   end
+   nn.close(pub_sock)
+end)
+sched()
+sp:close()
+sys.waitpid(pid)
