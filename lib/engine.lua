a1b2038441ff271ae427f5407eaf93d58ab539a0
Ruzsa Balazs <cellux@omkamra.hu>
2015-09-11 12:31:58 +0200

    ensure that SDL_GL_DOUBLEBUFFER is always set of OpenGLApps

--- lib/engine.lua
+++ lib/engine.lua
@@ -39,6 +39,11 @@ function SDLApp_mt:run()
          sdl.GL_SetAttribute(sdl.SDL_GL_CONTEXT_MINOR_VERSION, tonumber(minor))
       end
 
+      if self.gl_doublebuffer ~= nil then
+         sdl.GL_SetAttribute(sdl.SDL_GL_DOUBLEBUFFER,
+                             self.gl_doublebuffer and 1 or 0)
+      end
+
       -- create window
       local w = sdl.CreateWindow(self.title,
                                  self.x, self.y, 
@@ -142,6 +147,7 @@ function M.OpenGLApp(opts)
    self.fps = opts.fps or 60
    self.gl_profile = opts.gl_profile or 'core'
    self.gl_version = opts.gl_version or '3.3'
+   self.gl_doublebuffer = true
    return setmetatable(self, OpenGLApp_mt)
 end
 
