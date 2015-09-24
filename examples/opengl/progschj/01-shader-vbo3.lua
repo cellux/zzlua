cf15bba557518020e3c1770da5b561114e90b97a
Ruzsa Balazs <balazs.ruzsa@disney.com>
2015-09-24 10:39:56 +0200

    added OpenGL ES 2.0 version of an example

---
+++ examples/opengl/progschj/01-shader-vbo3.lua
@@ -0,0 +1,85 @@
+local engine = require('engine')
+local gl = require('gl')
+local ffi = require('ffi')
+
+local app = engine.OpenGLApp {
+   title = "shader-vbo1",
+   gl_profile = 'es',
+   gl_version = '2.0',
+}
+
+function app:init()
+   local FS = ffi.sizeof("GLfloat")
+
+   local vertex_shader = gl.CreateShader(gl.GL_VERTEX_SHADER)
+   vertex_shader:ShaderSource [[
+      #version 100
+      attribute vec4 vposition;
+      attribute vec4 vcolor;
+      varying lowp vec4 fcolor;
+      void main() {
+        fcolor = vcolor;
+        gl_Position = vposition;
+      }
+   ]]
+   vertex_shader:CompileShader()
+
+   local fragment_shader = gl.CreateShader(gl.GL_FRAGMENT_SHADER)
+   fragment_shader:ShaderSource [[
+      #version 100
+      varying lowp vec4 fcolor;
+      void main() {
+         gl_FragColor = fcolor;
+      }
+   ]]
+   fragment_shader:CompileShader()
+
+   local shader_program = gl.CreateProgram()
+   shader_program:AttachShader(vertex_shader)
+   shader_program:AttachShader(fragment_shader)
+   shader_program:BindAttribLocation(0, "vposition")
+   shader_program:BindAttribLocation(1, "vcolor")
+   --shader_program:BindFragDataLocation(0, "gl_FragColor")
+   shader_program:LinkProgram()
+
+   local vao = gl.VertexArray()
+   gl.BindVertexArray(vao)
+   local vbo = gl.Buffer()
+   gl.BindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+   local vertex_data = gl.FloatArray {
+   --   X    Y    Z          R    G    B
+       1.0, 1.0, 0.0,       1.0, 0.0, 0.0, -- vertex 0
+      -1.0, 1.0, 0.0,       0.0, 1.0, 0.0, -- vertex 1
+       1.0,-1.0, 0.0,       0.0, 0.0, 1.0, -- vertex 2
+       1.0,-1.0, 0.0,       0.0, 0.0, 1.0, -- vertex 3
+      -1.0, 1.0, 0.0,       0.0, 1.0, 0.0, -- vertex 4
+      -1.0,-1.0, 0.0,       1.0, 0.0, 0.0, -- vertex 5
+   } -- 6 vertices with 6 components (floats) each
+   gl.BufferData(gl.GL_ARRAY_BUFFER,
+                 ffi.sizeof(vertex_data), vertex_data,
+                 gl.GL_STATIC_DRAW)
+   gl.EnableVertexAttribArray(0)
+   gl.VertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 6*FS, 0*FS)
+   gl.EnableVertexAttribArray(1)
+   gl.VertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 6*FS, 3*FS)
+   gl.BindVertexArray(nil)
+
+   function app:draw()
+      gl.Clear(gl.GL_COLOR_BUFFER_BIT)
+      gl.UseProgram(shader_program)
+      gl.BindVertexArray(vao)
+      gl.DrawArrays(gl.GL_TRIANGLES, 0, 6)
+   end
+
+   function app:done()
+      vao:DeleteVertexArray()
+      vbo:DeleteBuffer()
+      shader_program:DetachShader(vertex_shader)
+      shader_program:DetachShader(fragment_shader)
+      vertex_shader:DeleteShader()
+      fragment_shader:DeleteShader()
+      shader_program:DeleteProgram()
+   end
+end
+
+app:run()
