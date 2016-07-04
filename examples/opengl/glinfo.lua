ed32273db154f5d31a12efafe3f5de5b04600366
Ruzsa Balazs <cellux@omkamra.hu>
2016-07-04 08:23:38 +0200

    print GL_SHADING_LANGUAGE_VERSION

--- examples/opengl/glinfo.lua
+++ examples/opengl/glinfo.lua
@@ -1,14 +1,14 @@
 local ffi = require('ffi')
 local sdl = require('sdl2')
-local sched = require('sched')
 local gl = require('gl')
+local sched = require('sched')
 
 local function getinfo()
    local w = sdl.CreateWindow("glinfo", -1, -1, nil, nil,
                               bit.bor(sdl.SDL_WINDOW_OPENGL,
                                       sdl.SDL_WINDOW_HIDDEN))
-   -- glGetString() returns valid information only after a valid
-   -- OpenGL context has been created, hence the hidden SDL window
+   -- glGetString() returns valid information only after an OpenGL
+   -- context has been created, hence the hidden SDL window
    local ctx = w:GL_CreateContext()
    local function p(name)
       pf("%s: %s", name, gl.GetString(gl[name]))
@@ -17,6 +17,7 @@ local function getinfo()
    p("GL_RENDERER")
    p("GL_VERSION")
    p("GL_EXTENSIONS")
+   p("GL_SHADING_LANGUAGE_VERSION")
    ctx:GL_DeleteContext()
    w:DestroyWindow()
 end
