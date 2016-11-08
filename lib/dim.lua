173587429078c8267e758da43373dc2823982c3a
Ruzsa Balazs <cellux@omkamra.hu>
2016-11-08 11:38:12 +0100

    dim.Size: add update() and clear()
    I'm not totally convinced that these are useful

--- lib/dim.lua
+++ lib/dim.lua
@@ -21,6 +21,15 @@ function Size_mt:__tostring()
    return sf("Size(%d,%d)", self.w, self.h)
 end
 
+function Size_mt:update(w,h)
+   self.w = w or self.w
+   self.h = h or self.h
+end
+
+function Size_mt:clear()
+   self:update(0,0)
+end
+
 M.Size = ffi.metatype("zz_dim_size", Size_mt)
 
 return M
