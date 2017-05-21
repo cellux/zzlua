55a507b9331d8dc8196ae9b18db3778146f11225
Ruzsa Balazs <cellux@omkamra.hu>
2017-05-21 18:25:12 +0200

    refactor sdl sched fd poller to use epoll and trigger

--- ext/sdl2.c
+++ ext/sdl2.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-#include <poll.h>
+#include <sys/epoll.h>
 
 #include <SDL2/SDL_events.h>
+
+#include "trigger.h"
 
 struct zz_sdl2_sched_fd_poller {
   uint32_t sched_fd_pollin_event_type;
   int sched_fd;
-  int exit_fd;
+  zz_trigger exit_trigger;
 };
 
 void *zz_sdl2_sched_fd_poller_thread(void *arg) {
@@ -14,36 +16,43 @@ void *zz_sdl2_sched_fd_poller_thread(void *arg) {
   SDL_Event sched_fd_pollin_event;
   SDL_zero(sched_fd_pollin_event);
   sched_fd_pollin_event.type = self->sched_fd_pollin_event_type;
-  struct pollfd pollfds[2];
-  /* sched_fd is the descriptor we shall monitor for readability */
-  pollfds[0].fd = self->sched_fd;
-  pollfds[0].events = POLLIN;
-  /* if we can read from exit_fd, the thread should exit */
-  pollfds[1].fd = self->exit_fd;
-  pollfds[1].events = POLLIN;
+  int epfd = epoll_create(1);
+  if (epfd < 0) {
+    fprintf(stderr, "sdl2_sched_fd_poller: epoll_create() failed\n");
+    exit(1);
+  }
+  struct epoll_event ev;
+  memset(&ev, 0, sizeof(ev));
+  ev.events = EPOLLIN;
+  ev.data.fd = self->sched_fd;
+  if (epoll_ctl(epfd, EPOLL_CTL_ADD, self->sched_fd, &ev)!=0) {
+    fprintf(stderr, "sdl2_sched_fd_poller: epoll_ctl() failed\n");
+    exit(1);
+  }
+  ev.events = EPOLLIN;
+  ev.data.fd = self->exit_trigger.fd;
+  if (epoll_ctl(epfd, EPOLL_CTL_ADD, self->exit_trigger.fd, &ev)!=0) {
+    fprintf(stderr, "sdl2_sched_fd_poller: epoll_ctl() failed\n");
+    exit(1);
+  }
   while (1) {
-    int status = poll(pollfds, 2, -1);
+    int status = epoll_wait(epfd, &ev, 1, -1);
     if (status <= 0) {
-      fprintf(stderr, "sdl2: poll() failed: status=%d\n", status);
+      fprintf(stderr, "sdl2_sched_fd_poller: epoll_wait() failed: status=%d\n", status);
       exit(1);
     }
-    if (pollfds[0].revents & POLLIN) {
+    if (ev.data.fd == self->sched_fd && (ev.events & EPOLLIN)) {
       SDL_PushEvent(&sched_fd_pollin_event);
     }
-    if (pollfds[1].revents & POLLIN) {
-      /* got the exit signal, leave the loop */
-      uint64_t exit_signal;
-      int nbytes = read(pollfds[1].fd, &exit_signal, 8);
-      if (nbytes != 8) {
-        fprintf(stderr, "sdl2: read(exit_fd) failed: nbytes=%d\n", nbytes);
-        exit(1);
-      }
-      if (exit_signal != 1) {
-        fprintf(stderr, "sdl2: read(exit_fd) failed: exit_signal=%lld\n", exit_signal);
-        exit(1);
-      }
+    else if (ev.data.fd == self->exit_trigger.fd) {
+      zz_trigger_poll(&self->exit_trigger); /* ack */
       break;
     }
+    else {
+      fprintf(stderr, "sdl2_sched_fd_poller: invalid fd in epoll_event structure: %d\n", ev.data.fd);
+      exit(1);
+    }
   }
+  close(epfd);
   return NULL;
 }
